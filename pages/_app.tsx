a8c8a24874307c4a3fdfae39882ce01cc62efbdf
Satvik Kaurav <satvikkaurav10@gmail.com>
2022-10-01 13:22:09 +0530

    basic setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,51 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from "@mantine/core";
+import { useState } from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp(props: AppProps) {
+  const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          fontFamily: "Open Sans, sans serif",
+          spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
+          colorScheme: "light",
+          colors: {
+            // override dark colors to change them for all components
+            dark: [
+              '#d5d7e0',
+              '#acaebf',
+              '#8c8fa3',
+              '#666980',
+              '#4d4f66',
+              '#34354a',
+              '#2b2c3d',
+              '#1d1e30',
+              '#0c0d21',
+              '#01010a',
+            ],
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
 }
-
-export default MyApp
